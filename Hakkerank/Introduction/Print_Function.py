914fd32362b24c38f802765ce97e921180ff60b1
Tri Vo <114336964+vottri@users.noreply.github.com>
2023-02-03 21:33:03 +0700

    Create Print_Function.py

---
+++ Hakkerank/Introduction/Print_Function.py
@@ -0,0 +1,32 @@
+# Print_Function
+
+# Task
+# The included code stub will read an integer, , from STDIN.
+# Without using any string methods, try to print the following:
+
+# 123...n
+
+# Note that "..." represents the consecutive values in between.
+
+# Example
+# n = 5
+# Print the string 12345.
+
+# Input Format
+# The first line contains an integer n.
+
+# Constraints
+# 1 <= n <= 150
+
+# Output Format
+# Print the list of integers from 1 through n as a string, without spaces.
+
+# ===== Write your code here =====
+
+if __name__ == '__main__':
+    n = int(input())
+
+for i in range(1,n+1):
+    print(i, end="")
+
+# source: https://www.geeksforgeeks.org/gfact-50-python-end-parameter-in-print/
