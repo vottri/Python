ef7df9db6bdfb45bfef59bab3f235bf5671b5172
Tri Vo <114336964+vottri@users.noreply.github.com>
2023-02-03 21:43:58 +0700

    Add files via upload

---
+++ Hakkerank/Problem_Solving/Solve_Me_First.py
@@ -0,0 +1,30 @@
+# Complete the function solveMeFirst to compute the sum of two integers.
+
+# Example
+# a = 7
+# b = 3
+# Return 10.
+
+# Function Description
+
+# Complete the solveMeFirst function in the editor below.
+# solveMeFirst has the following parameters:
+# - int a: the first value
+# - int b: the second value
+# Returns
+# - int: the sum of a and b.
+
+# Constraints
+# 1 <= a,b <= 1000
+
+# ===== Write your code here =====
+
+def solveMeFirst(a, b):
+    return a + b
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1, num2)
+
+print(res)
+
