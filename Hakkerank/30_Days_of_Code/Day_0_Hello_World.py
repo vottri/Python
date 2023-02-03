0b58398144984d29bd146e7cfa4ff603aae7b265
Tri Vo <114336964+vottri@users.noreply.github.com>
2023-02-03 14:24:27 +0700

    Add files via upload

---
+++ Hakkerank/30_Days_of_Code/Day_0_Hello_World.py
@@ -0,0 +1,19 @@
+# Task 
+# To complete this challenge, you must save a line of input from stdin to a variable, print "Hello, World."" on a single line, and finally
+# print the value of your variable on a second line.
+
+# Input Format
+# A single line of text denoting input_string (the variable whose contents must be printed).
+
+# Output Format
+# Print Hello, World. on the first line, and the contents of input_string on the second line.
+
+
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+
+# Write a line of code here that prints the contents of input_string to stdout.
+print(input_string)
