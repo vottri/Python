967e49b8768c382da9e04cdd42aa15534d343f85
Tri Vo <114336964+vottri@users.noreply.github.com>
2023-02-04 22:54:44 +0700

    Update Arithmetic_Operators.py

--- Hakkerank/Introduction/Arithmetic_Operators.py
+++ Hakkerank/Introduction/Arithmetic_Operators.py
@@ -2,9 +2,9 @@
 
 # Task 
 # Read two integers from STDIN and print three lines where:
-# The first line contains the sum of the two numbers.
-# The second line contains the difference of the two numbers (first - second).
-# The third line contains the product of the two numbers.
+# 1. The first line contains the sum of the two numbers.
+# 2. The second line contains the difference of the two numbers (first - second).
+# 3. The third line contains the product of the two numbers.
 
 # Input Format
 # The first line contains the first integer, a. The second line contains the second integer, b.
@@ -21,7 +21,12 @@
 if __name__ == '__main__':
     a = int(input())
     b = int(input())
-    
+
+# The first line contains the sum of the two numbers.
 print(a + b)
+
+# The second line contains the difference of the two numbers (first - second).
 print(a - b)
+
+# The third line contains the product of the two numbers.
 print(a * b)
